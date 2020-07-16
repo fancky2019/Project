e34deaa7cb6255478037819680cc48f456d5d9de
fancky <709737588@qq.com>
2020-07-16 17:56:55 +0800

    整理

--- zd/C#/QuickFixN/Client/FrmTradeClient.cs
+++ zd/C#/QuickFixN/Client/FrmTradeClient.cs
@@ -16,11 +16,47 @@ namespace Client
         public FrmTradeClient()
         {
             InitializeComponent();
+            this.btnStop.Enabled = false;
         }
         private void btnStart_Click(object sender, EventArgs e)
         {
 
             TradeClient.Instance.SocketInitiator.Start();
+            TradeClient.Instance.Logon += (msg =>
+              {
+                  if(this.InvokeRequired)
+                  {
+                      this.BeginInvoke((MethodInvoker)(() =>
+                      {
+                          this.btnStart.Enabled = false;
+                          this.btnStop.Enabled = true;
+                      }));
+                  }
+                  else
+                  {
+                      this.btnStart.Enabled = false;
+                      this.btnStop.Enabled = true;
+                  }
+             
+              });
+
+            TradeClient.Instance.LogOut += (msg =>
+            {
+                if (this.InvokeRequired)
+                {
+                    this.BeginInvoke((MethodInvoker)(() =>
+                    {
+                        this.btnStart.Enabled = true;
+                        this.btnStop.Enabled = false;
+                    }));
+                }
+                else
+                {
+                    this.btnStart.Enabled = true;
+                    this.btnStop.Enabled = false;
+                }
+
+            });
         }
 
         private void btnOpenDic_Click(object sender, EventArgs e)
