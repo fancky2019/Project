13a41699198aea71659217388f4b8f5eca38ca1f
fancky <709737588@qq.com>
2020-08-08 19:00:46 +0800

    配置文件整理

--- zd/C#/QuickFixN.TT/ZDFixService/Configuration.cs
+++ zd/C#/QuickFixN.TT/ZDFixService/Configuration.cs
@@ -5,29 +5,29 @@ using System.Text;
 
 namespace ZDFixService
 {
+    /*
+     * NuGet
+     * Microsoft.Extensions.Configuration
+     * Microsoft.Extensions.Configuration.FileExtensions
+     * Microsoft.Extensions.Configuration.Json
+     * */
     class Configurations
     {
         internal static IConfiguration Configuration { get; private set; }
-        /*
-         * NuGet
-         * Microsoft.Extensions.Configuration
-         * Microsoft.Extensions.Configuration.FileExtensions
-         * Microsoft.Extensions.Configuration.Json
-         * */
+
         static Configurations()
         {
+            LoadConfiguration();
+        }
+
+        private static void LoadConfiguration()
+        {
             var builder = new ConfigurationBuilder();
-            //将配置节点添加到内存
-            //builder.AddInMemoryCollection();
             builder.SetBasePath(AppContext.BaseDirectory);
             //设置配置文件
             builder.AddJsonFile("appsettings.json");
-
             Configuration = builder.Build();
-            //添加key到内存
-            //configuration["con"] = "val";   
-            var str = Configurations.Configuration["ZDFixService:OrderIDFilePath"];
-
+            //var str = Configurations.Configuration["ZDFixService:OrderIDFilePath"];
         }
     }
 }
